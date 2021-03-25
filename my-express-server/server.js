240542eb2bdba89c04a0183b245c42bee8dc9d28
Emma Paris <anmolpal1999@gmail.com>
2021-03-25 09:33:08 +0530

    setting up an express server

---
+++ my-express-server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request, response){
+  // console.log(request);
+  response.send("<h1>Hello! There.</h1>");
+});
+app.listen(3000, function(){
+  console.log("Server started on port 3000...");
+});
